6c40cb037db25896bf5a24e23c723aff8cdcca10
Parky Stevey <parkgrrr@gmail.com>
2017-02-22 14:50:32 -0700

    finished chapter 21, unit tests created, sounds work

---
+++ app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.beatbox;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pstev on 2/22/2017.
+ */
+public class SoundViewModelTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
