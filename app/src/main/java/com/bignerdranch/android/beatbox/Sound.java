1792b6cc38c00c8d8a1e3acabe1c2a8ef61026f3
Parky Stevey <parkgrrr@gmail.com>
2017-02-22 18:07:16 -0700

    Seekbar challenge in progress

--- app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -7,6 +7,7 @@ package com.bignerdranch.android.beatbox;
 public class Sound {
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
     public Sound(String assetPath){
         mAssetPath = assetPath;
         String[] components = assetPath.split("/");
@@ -18,6 +19,14 @@ public class Sound {
     }
     public String getName(){
         return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 
 }
--- app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.java
@@ -2,14 +2,19 @@ package com.bignerdranch.android.beatbox;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.databinding.ObservableField;
+import android.util.Log;
+import android.widget.SeekBar;
 
 /**
  * Created by pstev on 2/22/2017.
  */
 
-public class SoundViewModel extends BaseObservable {
+public class SoundViewModel extends BaseObservable{
     private Sound mSound;
     private BeatBox mBeatBox;
+    public ObservableField<String> seekBarValue = new ObservableField<>("");
+    private float mRate;
 
     public SoundViewModel(BeatBox beatBox) {
         mBeatBox = beatBox;
@@ -27,5 +32,17 @@ public class SoundViewModel extends BaseObservable {
     public void setSound(Sound sound){
         mSound = sound;
         notifyChange();
+    }
+
+    public void onButtonClicked() {
+        mBeatBox.play(mSound);
+    }
+
+    public void onValueChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+        Log.v("OVC", "On value change method called");
+        seekBarValue.set(progressValue + "");
+        int id = mSound.getSoundId();
+        mRate = ((float) progressValue);
+        mBeatBox.setRate(id, mRate);
     }
 }
